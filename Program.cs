e9ce54790bdc330f88041260c9e88aa45cd14b07
wanuchar <61377352+wanuchar@users.noreply.github.com>
2020-03-25 01:19:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Введите строку: ");
+            str = Console.ReadLine();
+
+            StringArray(str);
+        }
+
+        static void StringArray(string str) {
+            string result = string.Join(" ", str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] temp = result.Split(' ');
+
+            for (int i = temp.Length - 1; i >= 0; i--) {
+                Console.Write(temp[i] + " ");
+            }
+        }
+
+    }
+}
