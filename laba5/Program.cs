3924945a2523182af15e0c5f1d260804f045eb46
wanuchar <61377352+wanuchar@users.noreply.github.com>
2020-04-20 20:37:15 +0300

    Add files via upload

---
+++ laba5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab5
+{
+
+    class Program
+    {
+        static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.Write("Incorrect input. Try again: ");
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+        
+            Spec studSpec = new Spec("Ян", 17, "Беларусь", (courseNum)1, "ИиТП", "математика, информатика");
+            studSpec.getInfo();
+
+
+        }
+    }
+}
---
+++ laba5/Spec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Lab5
+{
+    class Spec : Student
+    {
+        public struct specialty {
+           public string specName;
+           public string mainSub;
+
+        }
+
+        public specialty specStruct;
+
+        public Spec() : this("Неизвестно",0, "Неизвестно", 0, "Неизвестно", "Неизвестно")
+        {
+            ID = 0;
+        }
+        public Spec(string a, int b, string c, courseNum f, string g, string h)
+        {
+            name = a;
+            age = b;
+            country = c;
+            ID = generateID();
+            course = (int)f;
+            specStruct.specName = g;
+            specStruct.mainSub = h;
+        }
+
+        public override void getInfo()
+        {
+            base.getInfo();
+            Console.WriteLine($"Специальность: {specStruct.specName}");
+            Console.WriteLine($"Профильный предмет: {specStruct.mainSub}");
+        }
+    }
+}
