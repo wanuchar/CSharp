ea93126b50bd176004b28a6a971515dfa194aa82
wanuchar <61377352+wanuchar@users.noreply.github.com>
2020-04-22 14:49:49 +0300

    Add files via upload

---
+++ laba8/IAvMark.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Lab8
+{
+    public interface IAvMark
+    {
+        static int[] arrayMark = new int[3];
+
+        double avMark();
+        void checkProgress(double av);
+    }
+}
