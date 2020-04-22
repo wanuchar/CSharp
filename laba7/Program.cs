0c754c358d34f621b61b27184aab5a648567d394
wanuchar <61377352+wanuchar@users.noreply.github.com>
2020-04-22 14:48:17 +0300

    Add files via upload

---
+++ laba7/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number number: ");
+            var num1 = Enter();
+            Console.WriteLine($"{num1.ShowFull()}");
+
+            Console.WriteLine("Enter the second number number: ");
+            var num2 = Enter();
+            Console.WriteLine($"{num2.ShowFull()}");
+
+            Console.WriteLine($"1. {num1.FractionStr()} > {num2.FractionStr()} = " + (num1 > num2));
+            Console.WriteLine($"   {num1.RNumStr()} > {num2.RNumStr()} = " + (num1 > num2)+ '\n');
+            Console.WriteLine($"2. {num1.FractionStr()} < {num2.FractionStr()} = " + (num1 < num2));
+            Console.WriteLine($"   {num1.RNumStr()} < {num2.RNumStr()} = " + (num1 < num2) + '\n');
+            Console.WriteLine($"3. {num1.FractionStr()} >= {num2.FractionStr()} = " + (num1 >= num2));
+            Console.WriteLine($"   {num1.RNumStr()} >= {num2.RNumStr()} = " + (num1 >= num2) + '\n');
+            Console.WriteLine($"4. {num1.FractionStr()} <= {num2.FractionStr()} = " + (num1 <= num2));
+            Console.WriteLine($"   {num1.RNumStr()} <= {num2.RNumStr()} = " + (num1 <= num2) + '\n');
+            Console.WriteLine($"5. {num1.FractionStr()} == {num2.FractionStr()} = " + (num1 == num2));
+            Console.WriteLine($"   {num1.RNumStr()} == {num2.RNumStr()} = " + (num1 == num2) + '\n');
+            Console.WriteLine($"6. {num1.FractionStr()} != {num2.FractionStr()} = " + (num1 != num2));
+            Console.WriteLine($"   {num1.RNumStr()} != {num2.RNumStr()} = " + (num1 != num2) + '\n');
+            Console.WriteLine($"7. {num1.FractionStr()} + {num2.FractionStr()} = " + (num1 + num2).FractionStr());
+            Console.WriteLine($"   {num1.RNumStr()} + {num2.RNumStr()} = " + (num1 + num2).RNumStr() + '\n');
+            Console.WriteLine($"8. {num1.FractionStr()} - {num2.FractionStr()} = " + (num1 - num2).FractionStr());
+            Console.WriteLine($"   {num1.RNumStr()} - {num2.RNumStr()} = " + (num1 - num2).RNumStr() + '\n');
+            Console.WriteLine($"9. {num1.FractionStr()} * {num2.FractionStr()} = " + (num1 * num2).FractionStr());
+            Console.WriteLine($"   {num1.RNumStr()} * {num2.RNumStr()} = " + (num1 * num2).RNumStr() + '\n');
+            if (num2 != 0)
+            {
+                Console.WriteLine($"10. {num1.FractionStr()} / {num2.FractionStr()} = " + (num1 / num2).FractionStr());
+                Console.WriteLine($"   {num1.RNumStr()} / {num2.RNumStr()} = " + (num1 / num2).RNumStr() + '\n');
+            }
+        }
+
+        public static RNum Enter()
+        {
+            int a;
+            Console.WriteLine("(0 - fraction, 1 - number): ");
+            while (!int.TryParse(Console.ReadLine(), out a) || a < 0 || a > 1)
+            {
+                Console.WriteLine("Incorrect input. Try again.\n");
+            }
+            if (a == 0)
+            {
+                return GetFraction();
+            }
+            else
+            {
+                return GetNum();
+            }
+
+        }
+
+        public static RNum GetFraction()
+        {
+            Console.WriteLine("Fraction: ");
+            string str = Console.ReadLine();
+            while (!CheckFraction(str))
+            {
+                Console.Write("Incorrect input, repeat: ");
+                str = Console.ReadLine();
+            }
+
+            RNum r = RNum.GetFractionFromStr(str);
+            return r;
+        }
+
+        public static bool CheckFraction(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i == 0 && str[i] == '-') i++;
+                if (str[i] == '/' && i != str.Length - 1) i++;
+                if (!char.IsDigit(str[i]) || (str[i] == '0' && i == str.Length - 1 && str[i - 1] == '/')) return false;
+            }
+            return true;
+        }
+
+        public static RNum GetNum()
+        {
+            Console.WriteLine("Number: ");
+            string str = Console.ReadLine();
+            double a = CheckNum(str);
+
+            RNum r = RNum.GetNumberFromStr(a);
+            return r;
+        }
+
+        public static double CheckNum(string str)
+        {
+            double a;
+            while(!double.TryParse(str, out a))
+            {
+                Console.Write("Incorrect input, repeat: ");
+                str = Console.ReadLine();
+            }
+            return a;
+        }
+
+    }
+}
---
+++ laba7/RNum.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace Lab7
+{
+    public class RNum : IComparable, IEquatable<RNum>
+    {
+        public int Integer { get; set; }
+        public int Natural { get; set; }
+
+        public RNum() : this(1, 1)
+        {
+        }
+        public RNum(int i, int n)
+        {
+            Integer = i;
+            Natural = n;
+        } 
+
+
+
+        public static RNum operator +(RNum r1, RNum r2)
+        {
+            RNum r = new RNum();
+            r.Integer = r1.Integer * r2.Natural + r2.Integer * r1.Natural;
+            r.Natural = r1.Natural * r2.Natural;
+            return r;
+        }
+
+        public static RNum operator -(RNum r1, RNum r2)
+        {
+            RNum r = new RNum();
+            r.Integer = r1.Integer * r2.Natural - r2.Integer * r1.Natural;
+            r.Natural = r1.Natural * r2.Natural;
+            return r;
+        }
+
+        public static RNum operator *(RNum r1, RNum r2)
+        {
+            RNum r = new RNum();
+            r.Integer = r1.Integer*r2.Integer;
+            r.Natural = r1.Natural * r2.Natural;
+            return r;
+        }
+
+        public static RNum operator /(RNum r1, RNum r2)
+        {
+            RNum r = new RNum();
+            r.Integer = r1.Integer*r2.Natural;
+            r.Natural = r1.Natural * r2.Integer;
+            return r;
+        }
+
+        public static bool operator >(RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) > ((double)r2.Integer / r2.Natural);
+        public static bool operator < (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) < ((double)r2.Integer / r2.Natural);
+        public static bool operator >= (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) >= ((double)r2.Integer / r2.Natural);
+        public static bool operator <= (RNum r1, RNum r2) => ((double)r1.Integer / r2.Natural) <= ((double)r2.Integer / r2.Natural);
+        public static bool operator == (RNum r1, RNum r2) => ((double)r1.Integer / r1.Natural).Equals((double)r2.Integer / r2.Natural);
+        public static bool operator != (RNum r1, RNum r2) => !((double)r1.Integer / r1.Natural).Equals((double)r2.Integer / r2.Natural);
+        public static bool operator !=(RNum r1, int r2) => !((double)r1.Integer / r1.Natural).Equals(r2);
+        public static bool operator ==(RNum r1, int r2) => ((double)r1.Integer / r1.Natural).Equals(r2);
+
+
+        public override bool Equals(object obj) => (double)((RNum)obj).Integer / ((RNum)obj).Natural == (double)this.Integer / this.Natural;
+        public override int GetHashCode() => this.GetHashCode();
+
+        public static implicit operator int (RNum r) => (int)(r.Integer / r.Natural);
+        public static implicit operator double(RNum r) => (double)r.Integer / r.Natural;
+
+        bool IEquatable<RNum>.Equals(RNum other) => (this.Integer == other.Integer && this.Natural == other.Natural);
+        int IComparable.CompareTo(object obj)
+        {
+            if ((this.Integer / this.Natural) > ((RNum)obj).Integer / ((RNum)obj).Natural)
+            {
+                return 1;
+            }
+            if ((this.Integer / this.Natural) < ((RNum)obj).Integer / ((RNum)obj).Natural)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public string RNumStr()
+        {
+            return $"{(double)Integer / Natural}";
+        }
+        public string FractionStr()
+        {
+            return $"{Integer}/{Natural}";
+        }
+        public string ShowFull()
+        {
+            return "Number: " + RNumStr() + "\nFraction: " + FractionStr() + "\n";
+        }
+
+        public static RNum GetFractionFromStr(string str)
+        {
+            RNum r = new RNum();
+
+            try
+            {
+                string[] array = str.Split('/');
+
+                r.Integer = int.Parse(array[0]);
+                r.Natural = int.Parse(array[1]);
+            }
+            catch
+            {
+                r.Integer = int.Parse(str);
+                r.Natural = 1;
+            }
+
+            return r;
+        }
+
+        public static RNum GetNumberFromStr(double a)
+        {
+            RNum r = new RNum();
+
+            int count = -1;
+            if (a % 10 == 0)
+            {
+                r.Integer = (int)a;
+                r.Natural = 1;
+            }
+            else
+            {
+                for (int i = 0; (float)a % 10 != 0 || Math.Abs(a) < 1; i++)
+                {
+                    a *= 10;
+                    count++;
+                }
+                a /= 10;
+                r.Integer = (int)a;
+                r.Natural = (int)Math.Pow(10, count);
+
+            }
+            return r;
+        }
+
+    }
+}
